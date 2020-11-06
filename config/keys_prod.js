c9fe7df52b46ccedb7720f7a62d9c77c2673508b
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-11-06 13:39:11 -0800

    prepared to deploy

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:process.env.MONGO_URI,
+  secretOrKey:process.env.SECRET_OR_KEY,
+  smtp:process.env.SMTP,
+  ConnexionToken:process.env.CONNEXION_TOKEN
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const posts = require('./routes/api/posts');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 const nodemailer = require("nodemailer");
 const lodash = require("lodash");
 const app = express();
@@ -20,7 +21,14 @@ app.get('/', (req, res) => res.send('Hello'));
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
-const port = 7200;
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+const port = process.env.PORT || 7200;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 //Db config
