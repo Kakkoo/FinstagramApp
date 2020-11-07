d8caa6586a1ca77dba2310a7b7bccfbc9398ab84
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-11-06 19:59:41 -0800

    keys added

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,8 @@ module.exports = {
   mongoURI:process.env.MONGO_URI,
   secretOrKey:process.env.SECRET_OR_KEY,
   smtp:process.env.SMTP,
-  ConnexionToken:process.env.CONNEXION_TOKEN
+  
+ cloud_name:process.env.CLOUD_NAME,
+ api_key:process.env.API_KEY,
+ api_secret:process.env.API_SECRET,
 };
